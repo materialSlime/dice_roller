e7a5544fa94df3fff0ecca027a1227b54309e296
turing_tested <materialSlime@users.noreply.github.com>
2024-03-31 12:33:37 +0530

    Now Points of dices generated dynmically after each refresh making old property to not stick when positioning points

--- index.js
+++ index.js
@@ -1,50 +1,69 @@
-let diceRoll1 = Math.floor(Math.random() * 6) + 1;;
-let diceRoll2 = Math.floor(Math.random() * 6) + 1;
+let diceRolls = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
 let diceLable = document.querySelector(".dice-lable");
 let resultTags = document.querySelectorAll(".result");
+let root = document.documentElement;
 
-function showDice(diceNo, dicePoints, result) {
-    let root = document.documentElement;
-    switch (dicePoints) {
+
+function steGridSize(diceNo, points) {
+    // root.style.setProperty(`--grid${diceNo}-size-x`, "3");
+    // root.style.setProperty(`--grid${diceNo}-size-y`, "3");
+    switch (points) {
         case 1:
             root.style.setProperty(`--grid${diceNo}-size-x`, "1");
             root.style.setProperty(`--grid${diceNo}-size-y`, "1");
             break;
         case 2:
-            document.querySelector(".d1 .dice-points-container div:first-child").style.gridRow = "3";
-            document.querySelector(".d1 .dice-points-container div:first-child").style.gridColumn = "3";
+            document.querySelector(`.d${diceNo} .dice-points-container div:last-child`).style.gridColumn = 3;
+            document.querySelector(`.d${diceNo} .dice-points-container div:last-child`).style.gridRow = 3;
+            break;
         case 3:
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(2)`).style.gridColumn = 2;
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(2)`).style.gridRow = 2;
+            document.querySelector(`.d${diceNo} .dice-points-container div:last-child`).style.gridColumn = 3;
+            document.querySelector(`.d${diceNo} .dice-points-container div:last-child`).style.gridRow = 3;
             break;
         case 4:
             root.style.setProperty(`--grid${diceNo}-size-x`, "2");
             root.style.setProperty(`--grid${diceNo}-size-y`, "2");
             break;
         case 5:
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(2)`).style.gridColumn = 3;
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(2)`).style.gridRow = 1;
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(3)`).style.gridColumn = 2;
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(3)`).style.gridRow = 2;
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(4)`).style.gridColumn = 1;
+            document.querySelector(`.d${diceNo} .dice-points-container div:nth-child(4)`).style.gridRow = 3;
+            document.querySelector(`.d${diceNo} .dice-points-container div:last-child`).style.gridColumn = 3;
+            document.querySelector(`.d${diceNo} .dice-points-container div:last-child`).style.gridRow = 3;
             break;
         case 6:
             root.style.setProperty(`--grid${diceNo}-size-x`, "2");
             root.style.setProperty(`--grid${diceNo}-size-y`, "3");
             break;
     }
-    diceDots = document.querySelectorAll(`.d${diceNo} .dice-points-container div`);
-    for (n = 0; n < dicePoints; n++) {
-        diceDots[n].classList.add("show");
-    }
-    result.innerHTML = dicePoints;
 }
+
+function createPoints(noOfPoints) {
+    points = document.querySelectorAll(".dice-points-container");
+    for (n = 0; n < 2; n++) {
+        for (i = 0; i < noOfPoints[n]; i++) {
+            points[n].innerHTML += "<div></div>";
+        }
+        resultTags[n].innerHTML = noOfPoints[n];
+        steGridSize(n + 1, noOfPoints[n]);
+    }
+}
+
 
 
 // Win Logic ---------------------------
-if (diceRoll1 > diceRoll2) {
+if (diceRolls[0] > diceRolls[1]) {
     diceLable.innerHTML = "Player 1 Wins!";
 }
-else if (diceRoll1 < diceRoll2) {
+else if (diceRolls[0] < diceRolls[1]) {
     diceLable.innerHTML = "Player 2 Wins!";
 } else {
     diceLable.innerHTML = "Draw.";
 }
 
-
-
-showDice(1, diceRoll1, resultTags[0]);
-showDice(2, diceRoll2, resultTags[1]);
+createPoints(diceRolls);
